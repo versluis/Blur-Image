4052c1f153aa70a5ee2ed68707071233a629b661
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2015-01-11 10:23:20 -0500

    Initial Commit

---
+++ Blur Image/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blur Image
+//
+//  Created by Jay Versluis on 11/01/2015.
+//  Copyright (c) 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blur Image/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Blur Image
+//
+//  Created by Jay Versluis on 11/01/2015.
+//  Copyright (c) 2015 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
