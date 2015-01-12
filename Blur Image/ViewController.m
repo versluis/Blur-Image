b3ad177aaf8f3bd7301cfacb58ed3966369ee244
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2015-01-11 20:05:11 -0500

    added Blur and Vibrancy effect

--- Blur Image/ViewController.m
+++ Blur Image/ViewController.m
@@ -11,6 +11,7 @@
 @interface ViewController ()
 
 @property (strong, nonatomic) IBOutlet UIImageView *imageView;
+
 @end
 
 @implementation ViewController
@@ -19,7 +20,26 @@
     [super viewDidLoad];
     
     // show image
-    self.imageView.image = [UIImage imageNamed:@"boat1"];
+    self.imageView.image = [UIImage imageNamed:@"boat6"];
+
+    // create blur effect
+    UIBlurEffect *blur = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
+
+    // create vibrancy effect
+    UIVibrancyEffect *vibrancy = [UIVibrancyEffect effectForBlurEffect:blur];
+
+    // add blur to an effect view
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc]initWithEffect:blur];
+    effectView.frame = self.view.frame;
+
+    // add vibrancy to yet another effect view
+    UIVisualEffectView *vibrantView = [[UIVisualEffectView alloc]initWithEffect:vibrancy];
+    effectView.frame = self.view.frame;
+
+    // add both effect views to the image view
+    [self.imageView addSubview:effectView];
+    [self.imageView addSubview:vibrantView];
+    
 }
 
 - (void)didReceiveMemoryWarning {
